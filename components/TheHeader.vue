01b639ea55b79b499ed561562608d109c94bf880
Victor Velasquez <victorvelas.lst@gmail.com>
2025-05-04 20:22:26 -0500

    Corrección del menu

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,15 +1,30 @@
 <template>
     <header>
-        <section class="brand">
-
-        </section>
-        <nav>
-            <ul>
-                <NuxtLink to="/">Home</NuxtLink>
-                <NuxtLink to="/posts">Posts</NuxtLink>
-                <NuxtLink to="/about">About</NuxtLink>
-                <NuxtLink to="/contact">Contact</NuxtLink>
-            </ul>
-        </nav>
+        <div>
+            <section class="brand">
+                <NuxtLink to="/">
+                    {{ appName }}
+                </NuxtLink>
+            </section>
+            <nav>
+                <ul>
+                    <li>
+                        <NuxtLink to="/" class="app-link">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/posts" class="app-link">Posts</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about" class="app-link">About</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/contact" class="app-link">Contact</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
 </template>
+<script lang="ts" setup>
+    const appName = ref<string>('My App Name');
+</script>
